c26bf30c36b681455e4e6ae775fde0dbee44c805
Naved <naved1234@gmail.com>
2018-10-29 02:50:20 +0530

    code addition

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -2,8 +2,8 @@ import jwt from 'jsonwebtoken';
 import HttpStatus from 'http-status-codes';
 
 const authenticate = async (req, res, next) => {
-  const token = (req.headers["authorization"]).split(" ")[1];
   try {
+    const token = (req.headers["authorization"]).split(" ")[1];
     const decoded = await jwt.verify(token, process.env.SECRET_KEY);
     console.log('decoded', decoded);
     req.userData = decoded;
